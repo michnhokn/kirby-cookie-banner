6c6a80db7c4e24574221150cdef01ddd34ea9462
Michael Scheurich <michael_scheurich@web.de>
2020-05-11 11:04:29 +0200

    update Cookie Banner

---
+++ README.md
@@ -0,0 +1,8 @@
+# A Cookie Banner for Kirby3
+
+### Use in JavaScript
+`cookies:all` on `<body>` if all cookies are accepted
+
+`cookies:deny` on `<body>` if only essential cookies are accepted
+
+`cookies:custom` on `<body>` if some cookies are accepted
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ Kirby::plugin('michnhokn/cookie-banner', [
     'options' => [
         'content' => [
             'title' => 'Cookie Einstellungen',
-            'text' => 'Wir nutzen Cookies ... Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
+            'text' => 'Wir nutzen Cookies um Dir die bestmögliche Erfahrung zu bieten. Außerdem können wir damit das Verhalten der Benutzer analysieren um die Webseite stetig für Dich zu verbessern.',
             'denyAll' => 'Alle ablehnen',
             'acceptAll' => 'Alle annehmen',
             'save' => 'Einstellung speichern',
